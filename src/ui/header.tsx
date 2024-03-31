58df2975ed73b0722eab84b7f1e7e9ca44ff0dc8
Chris Mendes <chris@chrismendes.uk>
2024-03-31 14:44:47 +0100

    Tidied site header

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { IconGitHub, IconLinkedIn } from "@/ui/icons";
+import { fontOswald } from "@/ui/fonts";
 
 export const SiteHeader = () => (
   <div className="w-full items-center justify-between lg:flex">
     <div className="flex flex-col">
-      <p className="text-xl font-bold">Christopher Mendes</p>
-      <p>Frontend Developer</p>
+      <p className={`${fontOswald.className} text-3xl uppercase mb-0`}>Christopher Mendes</p>
+      {/* <p className="text-sm">Frontend Developer</p> */}
     </div>
     <div className="flex">
       <Link className="py-2 px-3 text-slate-500 hover:text-black" href="/">
@@ -26,12 +27,14 @@ export const SiteHeader = () => (
       <Link className="py-2 px-3 text-slate-500 hover:text-black" href="/contact">
         Get In Touch
       </Link>
-      <Link className="py-2 px-3 text-slate-500 hover:text-black" href="https://github.com/chrismendes/" target="_blank">
-        <IconGitHub />
-      </Link>
-      <Link className="py-2 px-3 text-slate-500 hover:text-black" href="https://www.linkedin.com/in/chrismendes/" target="_blank">
-        <IconLinkedIn />
-      </Link>
+      <div className="flex flex-row ml-4">
+        <Link className="py-2 px-2 flex items-center text-slate-500 hover:text-black" href="https://github.com/chrismendes/" target="_blank">
+          <IconGitHub />
+        </Link>
+        <Link className="py-2 px-2 flex items-center text-slate-500 hover:text-black" href="https://www.linkedin.com/in/chrismendes/" target="_blank">
+          <IconLinkedIn />
+        </Link>
+      </div>
     </div>
   </div>
 );
