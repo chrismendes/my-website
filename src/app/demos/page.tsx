2303b33647056c5a9b5e80457d47cd49f44b29f6
Chris Mendes <chris@chrismendes.uk>
2024-04-05 13:00:26 +0100

    Added tech icons to demos page

--- src/app/demos/page.tsx
+++ src/app/demos/page.tsx
@@ -1,3 +1,4 @@
+import type { Content } from '@prismicio/client'
 import { prismic } from "@/prismicio";
 import { PrismicRichText } from "@prismicio/react";
 import Link from "next/link";
@@ -8,7 +9,7 @@ import { IconPlay, IconGitHub } from "@/ui/icons";
 
 export default async function DemoPage() {
 
-  const page = await prismic.getByType("demo_page", {
+  const page = await prismic.getSingle<Content.DemoPageDocument>("demo_page", {
     graphQuery: `
       {
         demo_page {
@@ -27,10 +28,13 @@ export default async function DemoPage() {
       }
     `
   });
-  if (!page.results_size) {
+  if (!page || !page.data) {
     return notFound();
   }
-  const content = page.results[0].data;
+  const content: Content.DemoPageDocumentData = page.data;
+  
+console.clear();
+// console.log(content.demos[1]?.demo.data.tech_stack);
   
   return (
     <div className="flex flex-row gap-x-12">
@@ -38,24 +42,26 @@ export default async function DemoPage() {
         <h1 className={`mb-6`}>{content.page_title}</h1>
       </div>
       <div className="flex w-5/6 self-end bg-neutral-50 p-12">
-        <ul role="list" className="flex flex-col gap-y-12">
+        <ul role="list" className="flex flex-col gap-y-24">
           {content.demos?.map(({ demo }, index) => (
             <li className="flex flex-row gap-x-14 items-center" key={index}>
               {demo.data.picture &&
-                <div className="w-2/5 h-[240px] overflow-hidden shadow-lg mb-4 flex items-center">
+                <div className="w-1/2 h-[300px] overflow-hidden shadow-lg flex items-center">
                   <PrismicNextImage field={demo.data.picture} />
                 </div>
               }
-              <div className="w-3/5 flex flex-col gap-y-4">
+              <div className="w-1/2 flex flex-col gap-y-4">
                 <h2>{demo.data.title}</h2>
                 <PrismicRichText field={demo.data.description} />
-                {/* {demo.data.tech_stack?.map(({ tech }, index) => (
-                  <SkillIcon
-                    image={<PrismicNextImage field={tech.icon} key={index} />}
-                    label={tech.name}
-                    key={index}
-                  />
-                ))} */}
+                <div className="flex gap-x-4 mb-2">
+                  {demo.data.tech_stack?.filter(({ tech }) => tech?.data).map(({ tech }, index) => (
+                    <SkillIcon
+                      image={<PrismicNextImage field={tech.data.icon} key={index} height={30} />}
+                      label={tech.data.name}
+                      key={index}
+                    />
+                  ))}
+                </div>
                 <div className="flex gap-x-4">
                   {demo.data.demo &&
                     <Button asChild>
