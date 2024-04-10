d7a332f887548479ec85d0bdf1ce1375a1a6d1fa
Chris Mendes <chris@chrismendes.uk>
2024-04-10 17:33:24 +0100

    Added responsive styles to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,21 +18,21 @@ export default async function Home() {
   
   return (
     <>
-      <div className="flex w-full gap-x-12 items-center self-center -mt-10">
-        <div className="w-3/5">
+      <div className="flex flex-col lg:flex-row w-full gap-x-12 items-center self-center -mt-10">
+        <div className="lg:w-3/5 text-center lg:text-left">
           <h1 className="mb-6 text-6xl before:hidden after:hidden">{content.headline}</h1>
           <PrismicRichText field={content.intro} />
 
-          <div className="flex items-center gap-x-6 mt-12">
-            <span className="font-bold">Tech Stack</span>
+          <div className="flex flex-col xl:flex-row items-center lg:items-start xl:items-center gap-6 mt-12">
+            <span className="font-bold text-nowrap hidden lg:inline">Tech Stack</span>
             <ul
-              className="flex gap-x-4 lg:before:content-['{'] before:text-8xl before:text-slate-200 before:mt-[-10px] lg:after:content-['}'] after:text-8xl after:text-slate-200 after:mt-[-10px]"
+              className="flex w-full flex-wrap gap-4 justify-center lg:justify-start xl:justify-center before:hidden xl:before:block xl:before:content-['{'] before:text-8xl before:text-slate-200 before:mt-[-10px] after:hidden xl:after:block xl:after:content-['}'] after:text-8xl after:text-slate-200 after:mt-[-10px]"
               role="list"
             >
               {content.key_skills.map(({ tech }, index) => (
                 <li className="flex flex-col items-center justify-center gap-y-3 min-w-14" key={index}>
                   <SkillIcon
-                    image={<PrismicNextImage field={tech.data.icon} />}
+                    image={<PrismicNextImage field={tech.data.icon} className="h-[30px] lg:h-auto w-auto" />}
                     label={tech.data.name}
                   />
                 </li>
@@ -40,7 +40,7 @@ export default async function Home() {
             </ul>
           </div>
         </div>
-        <div className="flex flex-1 justify-end">
+        <div className="flex flex-1 justify-end max-w-80 lg:max-w-none mt-10 lg:mt-0">
           <PrismicNextImage field={content.picture} className="border shadow" width="500" />
         </div>
       </div>
