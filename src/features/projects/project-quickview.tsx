12c687f30397a04f0dc6af7d0d798251bfc01e7f
Chris Mendes <chris@chrismendes.uk>
2024-04-19 01:57:55 +0100

    Added tech to project quick view

--- src/features/projects/project-quickview.tsx
+++ src/features/projects/project-quickview.tsx
@@ -30,15 +30,30 @@ export const ProjectQuickView = ({ title, description, logo, tech, pictures, isO
             {logo}
             <h2 className="mb-0 text-base lg:text-xl">{title}</h2>
           </div>
-          <div className="flex justify-center">
-            {React.isValidElement(description) ?
-              <>{description}</>
-            :
-              typeof description === "string" &&
-                <p>{description}</p>
+          <div className="flex flex-col gap-y-6">
+            <div className="flex justify-center">
+              {React.isValidElement(description) ?
+                <>{description}</>
+              :
+                typeof description === "string" &&
+                  <p>{description}</p>
+              }
+            </div>
+            {(tech && tech.length > 0) &&
+              <div className="flex flex-row justify-center gap-x-8">
+                {tech.length && tech.map((tech, index) => {
+                  return (
+                  <SkillIcon
+                    icon={tech.icon}
+                    label={tech.name}
+                    key={index}
+                  />
+                  )
+                })}
+              </div>
             }
           </div>
-          <div className="flex justify-center gap-x-3 text-neutral-400 text-lg uppercase font-sans select-none">
+          <div className="flex justify-center gap-x-3 text-neutral-400 text-lg uppercase font-sans select-none mt-8">
             <CircleChevronDown />
             Scroll Down
           </div>
