fd236903a5f86e93d9beb879a7bc7afe14c958b6
Chris Mendes <chris@chrismendes.uk>
2026-03-16 15:38:27 +0000

    Fixed for excessive project carousel height + hidden tabs when 1x tab only

--- src/ui/tabbed-carousel.tsx
+++ src/ui/tabbed-carousel.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/ui/carousel";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 import { Monitor, Smartphone } from "lucide-react";
+import { cn } from "@/utils";
 
 export interface CarouselTab {
   tabLabel: string;
@@ -23,48 +24,53 @@ const tabIcons = {
   Smartphone: Smartphone,
 }
 
-export const TabbedCarousel = ({ carousels }: Props) => (
-  <Tabs defaultValue="tab-0" className="mt-20">
-    <div className="flex">
-      <TabsList className="mx-auto mb-2">
-        {carousels?.map((carousel, index) => {
-          if (carousel.tabIcon) {
-            const Icon = tabIcons[carousel.tabIcon];
-            return (
-              <TabsTrigger value={`tab-${index}`}>
-                {Icon &&
-                  <span className="mr-2">
-                    <Icon />
-                  </span>
-                }
-                {carousel.tabLabel}
-              </TabsTrigger>
-            );
-          }
-        })}
-      </TabsList>
-    </div>
-    {carousels?.map((carousel, index) => (
-      <TabsContent value={`tab-${index}`}>
-        <Carousel>
-          <CarouselContent>
-            {carousel.pictures.map((picture, index) => (
-              <CarouselItem key={index}>
-                <div className="flex justify-center">
-                  {React.isValidElement(picture) &&
-                    <>
-                    {React.cloneElement(picture, {
-                      className: "w-auto"
-                    })}
-                    </>
-                  } 
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselNavigation />
-        </Carousel>
-      </TabsContent>
-    ))}
-  </Tabs>
-);
+export const TabbedCarousel = ({ carousels }: Props) => {
+  const carouselsParsed = carousels.filter((c) => Object.keys(c.pictures[0]).length > 0)
+  
+  return (
+    <Tabs defaultValue="tab-0" className="mt-20">
+      
+      <div className="flex">
+        <TabsList className={cn("mx-auto mb-2", carouselsParsed.length < 2 && "hidden")}>
+          {carouselsParsed?.map((carousel, index) => {
+            if (carousel.tabIcon) {
+              const Icon = tabIcons[carousel.tabIcon];
+              return (
+                <TabsTrigger value={`tab-${index}`}>
+                  {Icon &&
+                    <span className="mr-2">
+                      <Icon />
+                    </span>
+                  }
+                  {carousel.tabLabel}
+                </TabsTrigger>
+              );
+            }
+          })}
+        </TabsList>
+      </div>
+      {carouselsParsed?.map((carousel, index) => (
+        <TabsContent value={`tab-${index}`}>
+          <Carousel>
+            <CarouselContent>
+              {carousel.pictures.map((picture, index) => (
+                <CarouselItem key={index}>
+                  <div className="flex justify-center">
+                    {React.isValidElement(picture) &&
+                      <>
+                      {React.cloneElement(picture, {
+                        className: "w-auto max-h-[700px]"
+                      })}
+                      </>
+                    } 
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselNavigation />
+          </Carousel>
+        </TabsContent>
+      ))}
+    </Tabs>
+  )
+};
